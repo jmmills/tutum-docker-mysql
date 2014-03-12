f007de0ee4e84631a969b4d431e9821e87010d8b
Jason M. Mills <jmmills@cpan.org>
2014-03-11 21:20:11 -0700

    use full variable name for password

--- profile_d_make_my_cnf.sh
+++ profile_d_make_my_cnf.sh
@@ -1,5 +1,5 @@
 if [ -n $DB_PORT ]
 then
 	perl -E 'say "[client]\nuser=admin\nprotocol=$1\nhost=$2\nport=$3" if $ARGV[0] =~ /(\S+):\/\/(\S+):(\d+)/' $DB_PORT >~/.my.cnf
-	echo pass=$DB_ENV_MYSQL_PASS >>~/.my.cnf
+	echo password=$DB_ENV_MYSQL_PASS >>~/.my.cnf
 fi
