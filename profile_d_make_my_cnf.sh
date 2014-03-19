a714829680bac3f50f51222bda6e0d8ea9ae37b2
Jason M. Mills <jmmills@cpan.org>
2014-03-19 15:56:22 -0700

    my.cnf auto-creation was a poor idea. This is easily made simpler by using things like skydns

--- profile_d_make_my_cnf.sh
+++
@@ -1,5 +0,0 @@
-if [ -n $DB_PORT ]
-then
-	perl -E 'say "[client]\nuser=admin\nprotocol=$1\nhost=$2\nport=$3" if $ARGV[0] =~ /(\S+):\/\/(\S+):(\d+)/' $DB_PORT >~/.my.cnf
-	echo password=$DB_ENV_MYSQL_PASS >>~/.my.cnf
-fi
